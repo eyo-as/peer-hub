ba0a150f75d30fe4461ea2144ea6b9aca4f9058c
eyo-as <eyosiyastumiso7@gmail.com>
2025-08-21 06:37:55 +0300

    creating reusable api service

---
+++ service/api.ts
@@ -0,0 +1,25 @@
+export const API_URL = process.env.NEXT_API_URL;
+
+async function request<T>(
+  endpoint: string,
+  options: RequestInit = {}
+): Promise<T> {
+  const token =
+    typeof window !== "undefined" ? localStorage.getItem("token") : null;
+
+  const headers: HeadersInit = {
+    "Content-Type": "application/json",
+    ...(token ? { Authorization: `Bearer ${token}` } : {}),
+    ...options.headers,
+  };
+
+  const res = await fetch(`${API_URL}${endpoint}`, { ...options, headers });
+
+  if (!res.ok) {
+    throw new Error((await res.json()).message || "API Error");
+  }
+
+  return res.json();
+}
+
+export default request;
